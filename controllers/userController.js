7aa7c12ff20a1dbf730ba3fbcd5f1c1f82d997cd
Karl David <karl_david@dlsu.edu.ph>
2021-12-28 22:52:00 +0800

    5th commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,22 +7,22 @@ const path = require('path');
 const mysql = require('mysql')
 app.set('views', path.join(__dirname, 'views'));
 
+//connect to SQL
+const connection = mysql.createConnection({
+    host: 'hermes-eye.c5rtjx1kw1dq.us-east-2.rds.amazonaws.com',
+    user: 'admin',
+    password: '2005B230bc',
+    database: 'hermes_eye'
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected!');
+});
+
 exports.getIndex = (req, res)=>{
 
-    //connect to SQL
-    const connection = mysql.createConnection({
-        host: 'hermes-eye.c5rtjx1kw1dq.us-east-2.rds.amazonaws.com',
-        user: 'admin',
-        password: '2005B230bc',
-        database: 'hermes_eye'
-    });
-
-    connection.connect((err) => {
-        if (err) throw err;
-        console.log('Connected!');
-    });
-
-    connection.query("SELECT * FROM trucks", (err, rows) => {
+    connection.query("SELECT * FROM users", (err, rows) => {
         if(err) throw err;
 
         console.log(rows);
